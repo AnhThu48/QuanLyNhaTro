8b49b47d8b7a569875b34d62943105038fdf9c8f
AnhThu48 <thuhuathai@gmail.com>
2021-08-15 20:48:06 +0700

    up code

---
+++ QuanLyNhaTro/QLNT/QLNT/HeThongGiaoDien/Main.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace QLNT.HeThongGiaoDien
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnPT = new System.Windows.Forms.Button();
+            this.btnKH = new System.Windows.Forms.Button();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panelLef = new System.Windows.Forms.Panel();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.panelButtom = new System.Windows.Forms.Panel();
+            this.btnDX = new System.Windows.Forms.Button();
+            this.btnHD = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPT
+            // 
+            this.btnPT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(139)))), ((int)(((byte)(167)))));
+            this.btnPT.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.btnPT.Location = new System.Drawing.Point(139, 331);
+            this.btnPT.Name = "btnPT";
+            this.btnPT.Size = new System.Drawing.Size(202, 41);
+            this.btnPT.TabIndex = 2;
+            this.btnPT.Text = "PHÒNG TRỌ ";
+            this.btnPT.UseVisualStyleBackColor = false;
+            this.btnPT.Click += new System.EventHandler(this.btnPT_Click);
+            // 
+            // btnKH
+            // 
+            this.btnKH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(139)))), ((int)(((byte)(167)))));
+            this.btnKH.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKH.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.btnKH.Location = new System.Drawing.Point(533, 331);
+            this.btnKH.Name = "btnKH";
+            this.btnKH.Size = new System.Drawing.Size(202, 41);
+            this.btnKH.TabIndex = 4;
+            this.btnKH.Text = "KHÁCH HÀNG";
+            this.btnKH.UseVisualStyleBackColor = false;
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(139)))), ((int)(((byte)(167)))));
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(878, 10);
+            this.panelTop.TabIndex = 9;
+            // 
+            // panelLef
+            // 
+            this.panelLef.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(139)))), ((int)(((byte)(167)))));
+            this.panelLef.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLef.Location = new System.Drawing.Point(0, 10);
+            this.panelLef.Name = "panelLef";
+            this.panelLef.Size = new System.Drawing.Size(10, 834);
+            this.panelLef.TabIndex = 10;
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(139)))), ((int)(((byte)(167)))));
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRight.Location = new System.Drawing.Point(868, 10);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(10, 834);
+            this.panelRight.TabIndex = 11;
+            // 
+            // panelButtom
+            // 
+            this.panelButtom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(139)))), ((int)(((byte)(167)))));
+            this.panelButtom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelButtom.Location = new System.Drawing.Point(10, 834);
+            this.panelButtom.Name = "panelButtom";
+            this.panelButtom.Size = new System.Drawing.Size(858, 10);
+            this.panelButtom.TabIndex = 12;
+            // 
+            // btnDX
+            // 
+            this.btnDX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(139)))), ((int)(((byte)(167)))));
+            this.btnDX.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDX.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.btnDX.Location = new System.Drawing.Point(533, 627);
+            this.btnDX.Name = "btnDX";
+            this.btnDX.Size = new System.Drawing.Size(202, 41);
+            this.btnDX.TabIndex = 14;
+            this.btnDX.Text = "ĐĂNG XUẤT";
+            this.btnDX.UseVisualStyleBackColor = false;
+            this.btnDX.Click += new System.EventHandler(this.btnDX_Click);
+            // 
+            // btnHD
+            // 
+            this.btnHD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(139)))), ((int)(((byte)(167)))));
+            this.btnHD.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.btnHD.Location = new System.Drawing.Point(139, 627);
+            this.btnHD.Name = "btnHD";
+            this.btnHD.Size = new System.Drawing.Size(202, 41);
+            this.btnHD.TabIndex = 13;
+            this.btnHD.Text = "HÓA ĐƠN ";
+            this.btnHD.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.label1.Location = new System.Drawing.Point(173, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(538, 63);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "QUẢN LÝ PHÒNG TRỌ";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::QLNT.Properties.Resources.DangXuat;
+            this.pictureBox3.Location = new System.Drawing.Point(557, 429);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(168, 192);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::QLNT.Properties.Resources.HoaDon;
+            this.pictureBox4.Location = new System.Drawing.Point(173, 436);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(135, 185);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::QLNT.Properties.Resources.KhachHang1;
+            this.pictureBox2.Location = new System.Drawing.Point(459, 85);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QLNT.Properties.Resources.home;
+            this.pictureBox1.Location = new System.Drawing.Point(124, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 230);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(238)))), ((int)(((byte)(231)))));
+            this.ClientSize = new System.Drawing.Size(878, 844);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDX);
+            this.Controls.Add(this.btnHD);
+            this.Controls.Add(this.panelButtom);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLef);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.btnKH);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnPT);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Trang chủ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPT;
+        private System.Windows.Forms.Button btnKH;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Panel panelLef;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Panel panelButtom;
+        private System.Windows.Forms.Button btnDX;
+        private System.Windows.Forms.Button btnHD;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ QuanLyNhaTro/QLNT/QLNT/HeThongGiaoDien/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNT.HeThongGiaoDien
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDX_Click(object sender, EventArgs e)
+        {
+            // chuyen qua man hinh Login
+            this.Hide();
+            Login f1 = new Login();
+            f1.ShowDialog();
+        }
+
+        private void btnPT_Click(object sender, EventArgs e)
+        {
+            // chuyen qua man hinh PhongTro
+            this.Hide();
+            PhongTro fPT = new PhongTro();
+            fPT.ShowDialog();
+        }
+    }
+}
